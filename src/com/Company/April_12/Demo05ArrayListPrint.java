728c7c03aba5f4511e48fc8622cfa395a6c8bfe9
wangchengrui <1872164629@qq.com>
2021-04-14 20:02:51 +0800

    2021-4-14

--- src/com/Company/April_12/Demo05ArrayListPrint.java
+++ src/com/Company/April_12/Demo05ArrayListPrint.java
@@ -1,16 +1,42 @@
 package com.Company.April_12;
 
+import java.util.ArrayList;
+
 /**
  * @ClassName Demo05ArrayListPrint
  * @Description TODO         题目：
  *                              定义以指定格式打印集合的方法（ArrayList类型作为参数），使用@分隔每个元素。
  *                              格式参照（元素@元素@元素）。
+ *                              System.out.println(list);      [10, 20, 30]
+ *                              printArrayList(list);          {10@20@30}
  *
  * @Author Alaric
  * @Date 2021/4/12
  **/
 public class Demo05ArrayListPrint {
     public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("林丹");
+    list.add("李宗伟");
+    list.add("盖德");
+    list.add("陶菲克");
+    System.out.println(list);
 
+    printArrayList(list);
+    }
+
+
+    public static void printArrayList(ArrayList<String> list) {
+        // 该方法打印输出{10@20@30】
+        System.out.print("{");
+        for (int i = 0; i < list.size(); i++) {
+            String name = list.get(i);
+            if (i == list.size() - 1) {
+                System.out.println(name + "}");
+            }
+            else {
+                System.out.print(name + "@");
+            }
+        }
     }
 }
---
+++ src/com/Company/April_12/Demo06ArrayListReturn.java
@@ -0,0 +1,22 @@
+package com.Company.April_12;
+
+/**
+ * @ClassName Demo06ArrayListReturn
+ * @Description TODO       题目：
+ *                         用一个大集合存入20个随机数字，然后筛选其中的偶数元素，放到小集合当中。
+ *                         要求使用自定义的方法乱实现筛选
+ *                         分析：
+ *                         1、需要创建一个大集合，用来存储int数字：<Integer>
+ *                         2、随机数字就用Random nextInt
+ *                         3、循环20次，吧随机数字放入大集合：for循环、add方法
+ *                         4、定义一个方法，用来进行筛选。
+ *                         筛选：根据大集合，筛选符合要求的元素，得到小集合。
+ *                         三要素
+ *                         返回值类型：ArrayList小集合（里面元素个数不确定）
+ *                         方法名称：getSmallList
+ *                         参数列表：ArrayList大集合（装着20个随机数字）
+ * @Author Alaric
+ * @Date 2021/4/14
+ **/
+public class Demo06ArrayListReturn {
+}
